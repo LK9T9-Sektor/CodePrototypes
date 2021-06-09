72c5fb2c30ac164f14be937ad7b92e123cd466b7
LK9T9-Sektor <lk9t9_sektor@mail.ru>
2021-06-09 12:34:21 +0700

    Added a template for using a Serilog with M. E. C and with a pre-prepared configuration file.

---
+++ SerilogM.E.C/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace SerilogM.E.C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var basedir = "BASEDIR";
+            Environment.SetEnvironmentVariable(basedir, Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+            var basePath = Environment.GetEnvironmentVariable(basedir);
+
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json", false, true);
+
+            var configuration = configurationBuilder.Build();
+
+            ILogger log = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            log.Information("App start");
+
+            log.Information("App end");
+        }
+    }
+}
